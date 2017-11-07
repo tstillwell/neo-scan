80033d611216a7befaa58b46e3b2a61f3fdabcba
tstillwell2k17 <tristanstillwell@gmail.com>
2017-11-06 21:44:17 -0500

    style: Add missing semicolon for $neo assignment

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ function loadData() {
 		  var $number_of_neos = neo_results.element_count;
 		  var $results = "<thead><tr><th>NEO Name</th><th>Min Est. Diameter (meters)</th><th>Max Est. Diameter (meters)</th><th>Miss Distance (km)</th></tr></thead><tbody>";
 		  for (var i = 0; i < $number_of_neos; i++) {
-			   $neo = neo_results.near_earth_objects[$date][i]
+			   $neo = neo_results.near_earth_objects[$date][i];
 			   $result_list_item = "<tr class='neo-item'><td>";
 			   $result_list_item += "<a href='" + $neo.nasa_jpl_url + "'>" + $neo.name + "</a></td>";
 			   $result_list_item += "<td>" + $neo.estimated_diameter.meters.estimated_diameter_min.toFixed(2) + "</td>";
