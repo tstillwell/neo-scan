72fc323c73225ba6312c038ce4150e4a33934bc9
tstillwell2k17 <tristanstillwell@gmail.com>
2017-11-08 20:45:55 -0500

    feat: Activate zebra widget to color alternating rows.

--- js/main.js
+++ js/main.js
@@ -27,7 +27,11 @@ function loadData() {
 			   }
 		  $results += "</tbody>";
 		  $("#nasa_results").html($results);
-		  $("#nasa_results").tablesorter();
+		  $("#nasa_results").tablesorter({
+			theme: 'blue',
+			widgets: ["zebra"]
+			});
+		  $("#nasa_results").trigger('applyWidgets')
 		}).fail(function(err) {
 		  var $error_msg = "<span>Nasa Data could not be loaded</span>";
 		  $("#nasa_results").html($error_msg);
