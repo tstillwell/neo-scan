fa61ae5771aa37bd2515335f59e4251d163e1550
tstillwell2k17 <tristanstillwell@gmail.com>
2017-12-05 21:39:39 -0600

    fix: Move api error text out of table

--- js/main.js
+++ js/main.js
@@ -33,8 +33,8 @@ function loadData() {
 			});
 		  $("#nasa_results").trigger('applyWidgets')
 		}).fail(function(err) {
-		  var $error_msg = "<span class='apierror'>Nasa Data could not be loaded</span>";
-		  $("#nasa_results").html($error_msg);
+		  var $error_msg = "<span>Nasa Data could not be loaded</span>";
+		  $(".apierror").html($error_msg);
 		  throw err;
 		});
 }
