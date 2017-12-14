84a1bd8b14f17879c5818ec5d0d26e88450cbc89
tstillwell2k17 <tristanstillwell@gmail.com>
2017-12-13 23:30:47 -0500

    style: Remove extra $ from 'neo' var name

--- js/main.js
+++ js/main.js
@@ -16,12 +16,12 @@ function loadData() {
 		  var $results = "<thead><tr><th>NEO Name</th><th>Min Est. Diameter (meters)</th>"
 		  $results += "<th>Max Est. Diameter (meters)</th><th>Miss Distance (km)</th></tr></thead><tbody>";
 		  for (var i = 0; i < number_of_neos; i++) { // Add a row to table for each NEO in response
-			   $neo = neo_results.near_earth_objects[$date][i];
+			   neo = neo_results.near_earth_objects[$date][i];
 			   $result_list_item = "<tr class='neo-item'><td class='neo-name'>";
-			   $result_list_item += "<a href='" + $neo.nasa_jpl_url + "'>" + $neo.name + "</a></td>";
-			   $result_list_item += "<td>" + $neo.estimated_diameter.meters.estimated_diameter_min.toFixed(2) + "</td>";
-			   $result_list_item += "<td>" + $neo.estimated_diameter.meters.estimated_diameter_max.toFixed(2) + " </td>";
-			   $result_list_item += "<td><span class='miss-distance'>" + Math.round($neo.close_approach_data[0].miss_distance.kilometers) + "</span></td>";
+			   $result_list_item += "<a href='" + neo.nasa_jpl_url + "'>" + neo.name + "</a></td>";
+			   $result_list_item += "<td>" + neo.estimated_diameter.meters.estimated_diameter_min.toFixed(2) + "</td>";
+			   $result_list_item += "<td>" + neo.estimated_diameter.meters.estimated_diameter_max.toFixed(2) + " </td>";
+			   $result_list_item += "<td><span class='miss-distance'>" + Math.round(neo.close_approach_data[0].miss_distance.kilometers) + "</span></td>";
 			   $result_list_item += "</tr>";
 			   $results += $result_list_item;
 			   }
