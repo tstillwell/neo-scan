d030d346b8d21b8e7acdc7e946b8493237ce4021
tstillwell2k17 <tristanstillwell@gmail.com>
2017-12-30 21:23:46 -0500

    style: Shorten line length in table building code

--- js/main.js
+++ js/main.js
@@ -15,12 +15,13 @@ function loadData() { // retrieve data from API and build table
 		  var size_unit = meters_selected() ? 'meters' : 'feet';
 		  var distance_unit = km_selected() ? 'kilometers' : 'miles';
 		  var result_table = "<thead><tr><th>NEO Name</th><th>Min Est. Diameter (" + size_unit + ")</th>";
-		  result_table += "<th>Max Est. Diameter (" + size_unit + ")</th><th>Miss Distance (" + distance_unit + ")</th></tr></thead><tbody>";
+		  result_table += "<th>Max Est. Diameter (" + size_unit + ")</th><th>Miss Distance (";
+		  result_table += distance_unit + ")</th></tr></thead><tbody>";
 		  for (var i = 0; i < number_of_neos; i++) { // Add a row to table for each NEO in response
 			   neo = result.near_earth_objects[date][i];
 			   result_list_item = "<tr class='neo-item'><td class='neo-name'>";
 			   result_list_item += "<a href='" + neo.nasa_jpl_url + "'>" + neo.name + "</a></td>";
-			   result_list_item += "<td>" + "<span class='diameter-min' data-dmin-feet='" + neo.estimated_diameter.feet.estimated_diameter_min.toFixed(2)
+			   result_list_item += "<td>" + "<span class='diameter-min' data-dmin-feet='" + neo.estimated_diameter.feet.estimated_diameter_min.toFixed(2);
 			   result_list_item + " data-dmin-meters='" + neo.estimated_diameter.meters.estimated_diameter_min.toFixed(2) + "'>";
 			   result_list_item += neo.estimated_diameter[size_unit].estimated_diameter_min.toFixed(2) + "</span></td>";
 			   result_list_item += "<td>" + "<span class='diameter-max'>";
