ee55d5d303c1e9b4d781c9df7157ba2e1c074b83
tstillwell <tristanstillwell@gmail.com>
2019-02-12 16:04:00 -0700

    style: Use node-style indent for todayDate function

--- js/neo-scan.js
+++ js/neo-scan.js
@@ -103,20 +103,20 @@ function km_selected() {  // true if km is selected
 }
 
 function todayDate() { // today's date in yyyy-mm-dd format
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth()+1;
-    var yyyy = today.getFullYear();
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1;
+  var yyyy = today.getFullYear();
 
-    if (dd<10) {
-        dd = "0"+ dd;
-    }
-    if (mm<10) {
-        mm = "0"+ mm;
-    }
-    today = yyyy + "-" + mm + "-" + dd;
-    return today;
-    }
+  if (dd<10) {
+    dd = "0"+ dd;
+  }
+  if (mm<10) {
+    mm = "0"+ mm;
+  }
+  today = yyyy + "-" + mm + "-" + dd;
+  return today;
+}
 
 
 $( "#date" ).datepicker();  // Add jqueryui datepicker to date control
